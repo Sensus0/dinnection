8e849d90e9837b6f8f9f64044be2aec5e6c230af
Gravity <tevoned@gmail.com>
2022-03-30 00:58:59 -0400

    user pro update

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.slider');
+        var instances = M.Slider.init(elems, options);
+      });
+    });
+    
+
+    var instance = M.Slider.getInstance(elem);
+
+ 
