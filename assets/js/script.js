31510a50d6b2a007bafee7f59f82d53711e26fea
Gravity <tevoned@gmail.com>
2022-03-31 01:11:03 -0400

    User profile update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
   });
 
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
@@ -9,4 +9,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Or with jQuery
 
-        
+ 
