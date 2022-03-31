09f1c85d03f13c21545fa4a07287f9d57931b856
Gravity <tevoned@gmail.com>
2022-03-30 21:34:14 -0400

    user profile mods

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.slider');
-        var instances = M.Slider.init(elems, options);
-      });
-    });
-    
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
 
-    var instance = M.Slider.getInstance(elem);
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
- 
+  // Or with jQuery
+
+        
