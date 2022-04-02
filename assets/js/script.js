2dea4a0cc67fe8ad437abfd805ab6d4948045fb8
Gravity <tevoned@gmail.com>
2022-04-02 16:31:28 -0400

    likes Button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
   });
 
+  let counterDisplayElem = document.querySelector('.counter-display');
+  let counterPlusElem = document.querySelector('.counter-plus');
+  
+  let count = 0;
+
+  updateDisplay();
+
+  counterPlusElem.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+}) ;
+
+
+
+
+
+function updateDisplay(){
+    counterDisplayElem.innerHTML = count;
+};
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
   // var collapsibleElem = document.querySelector('.collapsible');
   // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
